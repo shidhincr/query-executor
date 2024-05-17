6e670d226c0a0af8e2691ac2ff959cd553eb3392
Shidhin <shidhincr@gmail.com>
2024-05-17 22:49:14 +0530

    chore: install tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <h1 className="text-3xl font-bold underline text-sky-700">
+        Hello world!
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
